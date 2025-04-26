e00adbde57f705dbb64a733261fd8ea8b694395e
Danilo Franco <engdanilofranco@gmail.com>
2025-04-26 20:26:18 -0300

    Configuração do arquivo de roteamento .htaccess - Implemetanção do index.php - Inicio da implementação do UsuarioController.php

---
+++ api/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+//controladores
+use Controllers\UsuarioController;
+
+//Utils
+use Utils\HttpResponses;
+
+$url = isset($_GET['url']) ? $_GET['url'] : '';
+
+if (!empty($url)) {
+    $url = rtrim($url, '/');
+    $url_parts = explode('/', $url);
+    
+    $recurso = isset($url_parts[0]) ? $url_parts[0] : '';
+    
+    $acao = isset($url_parts[1]) ? $url_parts[1] : 'index';
+    
+    switch ($recurso) {
+        case 'usuario':            
+            $controller = new UsuarioController();//instanciar o controlardor aqui
+            break;          
+        default:
+            $controller = null;
+            break;
+    }
+    
+    if ($controller && method_exists($controller, $acao)) {
+        
+        $params = array_slice($url_parts, 2);
+        call_user_func_array([$controller, $acao], $params);
+    } else {
+        HttpResponses::notFound();
+    }
+} else {
+    HttpResponses::notFound();
+    exit;
+}
+
+?>
---
+++ api/src/controllers/UsuarioController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class UsuarioController{
+
+    function cadastrarusuario(){
+        echo "controlador funcionando";
+    }
+
+}
+
+?>
